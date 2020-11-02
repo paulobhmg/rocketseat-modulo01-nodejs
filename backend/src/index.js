a10a00dacf2e5647853d8c7415126b712fc567a0
Paulo Nogueira <paulobh.nogueira@gmail.com>
2020-11-01 23:44:53 -0300

    Configurando o nodemon para atualização automática do servidor

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,6 @@ express = require('express');
 app = express();
 
 app.get('/', (req, res) => res.send('Hello Word!'));
-app.get('/index', (req, res) => res.json({message: "Hello World!"}));
+app.get('/index', (req, res) => res.json({message: "Hello World, again!"}));
 
 app.listen(3333);
