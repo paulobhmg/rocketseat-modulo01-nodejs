3270f01dca577569c36ead72bc1340accddfa5e1
Paulo Nogueira <paulobh.nogueira@gmail.com>
2020-11-02 22:27:27 -0300

    Adicionando middlewares ao projeto

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 express = require('express');
 app = express();
 const {v4} = require('uuid');
+const {isUuid} = require('uuidv4');
 
-//Habilita o express para receber body params em formato JSON
+//Habilita o expresponses para receber body params em formato JSON
 app.use(express.json());
 
 let projects = [];
@@ -17,55 +18,78 @@ let projects = [];
  * Query Params - mais utilizados para filtros e paginação
  * Route Params - Utilizados para informar qual é o recurso que será utilizado
  * Body Params  - Utilizados para coletar as informações de formulários, etc
+ * 
+ * Middlewaresponse são interceptadoresponse que podem ou não bloquear a sequência de execução
+ * de um programa.
  */
 
- app.get('/projects', (req, res) => {
-   const {title} = req.query;
-   const results = title
+function myMiddleware(request, response, next){
+  const {method, url} = request;
+  const label = `[${method}]`.toUpperCase() + ` ${url}`;
+  console.log(label);
+  
+  return next();
+}
+
+function verifyRequestId(request, response, next){
+  const {id} = request.params;
+
+  if(!isUuid(id)){
+    return response.json({message: "Invalid request ID."});
+  }
+
+  return next();
+}
+
+app.use(myMiddleware);
+
+app.get('/projects', (request, response) => {
+  const {title} = request.query;
+  const responseults = title
     ? projects.filter(project => project.title.includes(title))
     : projects;
 
-   return res.json(results);
- });
+  return response.json(responseults);
+});
 
- app.post('/projects', (req, res) => {
-   const {title, owner} = req.body;
-   const project = {id: v4(), title, owner};
-   projects.push(project);
-   
-   return res.json(project);
- });
+app.post('/projects', (request, response) => {
+  const {title, owner} = request.body;
+  const project = {id: v4(), title, owner};
+  projects.push(project);
+  
+  return response.json(project);
+});
 
- app.put('/projects/:id', (req, res) => {
-   const {id} = req.params;
-   const {title, owner} = req.body;
-   const projectIndex = projects.findIndex(project => project.id === id);
+app.put('/projects/:id', verifyRequestId, (request, response) => {
+  const {id} = request.params;
+  const {title, owner} = request.body;
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-   if(projectIndex < 0){
-     return res.status(400).json({"message": "project not found"});
-   }
+  if(projectIndex < 0){
+    return response.status(400).json({"message": "project not found"});
+  }
 
-   const project = {id, title, owner};
-   projects[projectIndex] = project;
+  const project = {id, title, owner};
+  projects[projectIndex] = project;
 
-   return res.json(project);
- });
+  return response.json(project);
+});
 
- app.delete('/projects/:id', (req, res) => {
-   const {id} = req.params;
-   const projectIndex = projects.findIndex(project => project.id === id);
+app.delete('/projects/:id', verifyRequestId, (request, response) => {
+  const {id} = request.params;
+  const projectIndex = projects.findIndex(project => project.id === id);
 
-   if(projectIndex < 0){
-     return res.json({"message": "Project not founded"});
-   }
-   
-   const title = projects[projectIndex].title;
-   projects.splice(projectIndex, 1);
+  if(projectIndex < 0){
+    return response.json({"message": "Project not founded"});
+  }
+  
+  const title = projects[projectIndex].title;
+  projects.splice(projectIndex, 1);
 
-   return res.status(200).send(`Project '${title}' was removed.`);
- });
+  return response.status(200).send(`Project '${title}' was removed.`);
+});
 
-app.get('/', (req, res) => res.send('Hello Word!'));
-app.get('/index', (req, res) => res.json({message: "Hello World, again!"}));
+app.get('/', (request, response) => response.send('Hello Word!'));
+app.get('/index', (request, response) => response.json({message: "Hello World, again!"}));
 
 app.listen(3333);
